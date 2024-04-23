48cf78b9936e151ee78ef12400e408141a04458e
doregarr <siregarado01@gmail.com>
2024-04-23 10:49:11 +0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Two Cards</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="card">
+        <h2>Card 1</h2>
+        <p>This is the content of card 1.</p>
+    </div>
+    <div class="card">
+<span>
+        <img src="{{ asset('image/logo-pemko.jpeg') }}"></span>
+        <span><img src="{{ asset('image/kolaborasi-medan-berkah.png') }}"></span>
+    </div>
+</body>
+</html>
