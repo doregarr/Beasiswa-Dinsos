53b2e129e54ca850d7db77bffb18af6acf074237
doregarr <siregarado01@gmail.com>
2024-04-23 11:57:18 +0700

    asd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,19 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    <div class="card">
-        <h2>Card 1</h2>
-        <p>This is the content of card 1.</p>
-    </div>
-    <div class="card1">
-
-        <img src="{{ asset('image/logo-pemko.jpeg') }}" class="icon1">
-       <img src="{{ asset('image/kolaborasi-medan-berkah.png') }}" class="icon2">
+    <div class="card-container">
+        <div class="card">
+            <h2>Card 1</h2>
+            <p>This is the content of card 1.</p>
+        </div>
+        <div class="card1">
+            <img src="{{ asset('image/logo-pemko.png') }}" class="icon1">
+            <img src="{{ asset('image/kolaborasi-medan-berkah.png') }}" class="icon2">
+            <div class="box">
+                <h3>Box Inside Card 2</h3>
+                <p>This is the content of the box inside card 2.</p>
+            </div>
+        </div>
     </div>
 </body>
 </html>
