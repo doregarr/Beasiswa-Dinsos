57c83b83a3ae49bb9a4d421ba0fd2d13ee147ba1
johanasht <sihotangjohana@gmail.com>
2024-04-23 15:56:46 +0700

    update page 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,17 +9,59 @@
 <body>
     <div class="card-container">
         <div class="card">
-            <h2>Card 1</h2>
-            <p>This is the content of card 1.</p>
-        </div>
-        <div class="card1">
-            <img src="{{ asset('image/logo-pemko.png') }}" class="icon1">
-            <img src="{{ asset('image/kolaborasi-medan-berkah.png') }}" class="icon2">
-            <div class="box">
-                <h3>Box Inside Card 2</h3>
-                <p>This is the content of the box inside card 2.</p>
+            <h2>Pengajuan Bantuan Biaya Pendidikan</h2>
+            <p>Dinas Sosial Kota Medan</p>
+            <div class="card-inner">
+               <div class="table-container">
+                <table>
+                    <tbody>
+                    <tr class="table-head">
+                        <td>PENGUMUMAN BEASISWA GEL II</td>
+                        <td><button class="btn-unduh">Unduh</button></td>
+                    </tr>
+                    <tr class="table-head">
+                        <td>DATA MAHASISWA CALON PENERIMA BANTUAN BIAYA PENDIDIKAN</td>
+                        <td><button class="btn-unduh">Unduh</button></td>
+                    </tr>
+                    <tr class="table-head">
+                        <td>TEMPLATE SURAT PERMOHONAN DITUJUKAN KEPADA BAPAK WALI KOTA MEDAN Cq. KEPALA DINAS SOSIAL KOTA MEDAN</td>
+                        <td><button class="btn-unduh">Unduh</button></td>
+                    </tr>
+                    <tr class="table-head">
+                        <td>TEMPLATE SURAT PERNYATAAN TIDAK MENERIMA BEASISWA/BANTUAN BIAYA PENDIDIKAN DARI SUMBER LAIN</td>
+                        <td><button class="btn-unduh">Unduh</button></td>
+                    </tr>
+                    <tr class="table-head">
+                        <td>TEMPLATE SURAT PERNYATAAN TIDAK BERSTATUS SEBAGAI APARATUR SIPIL NEGARA(ASN)</td>
+                        <td><button class="btn-unduh">Unduh</button></td>
+                    </tr>
+                    </tbody>
+                </table>
+               </div>
             </div>
         </div>
+        <div class="card1">
+            <div class="box">
+                <img src="{{ asset('image/logo-pemko.png') }}" class="icon1">
+                <img src="{{ asset('image/kolaborasi-medan-berkah.png') }}" class="icon2">
+            </div>
+            <div class="box1">
+                <div class="text">
+<h3 class="login">Login</h3>
+<input type="text" placeholder="Masukkan Nomor Induk Kependudukan" class="input1">
+<br>
+
+<input type="password" placeholder="Masukkan Password" class="input2">
+<div class="btn-login">
+    <button type="submit" > Login</button>
+    <a href >Daftar Akun</a>
+</div>
+</div>
+
+</div>
+
+        </div>
+    </div>
     </div>
 </body>
 </html>
