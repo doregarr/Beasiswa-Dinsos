5b9977f3225a0c02285360335013008b2b9867ca
johanasht <sihotangjohana@gmail.com>
2024-04-24 15:19:04 +0700

    up

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Two Cards</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Borel&family=Lato:ital@1&family=Poppins:ital,wght@0,200;0,300;0,700;1,700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
